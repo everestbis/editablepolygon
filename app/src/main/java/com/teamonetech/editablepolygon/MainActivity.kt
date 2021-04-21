bc7e8d78ddeef01df94a6476bd18d417afbc1103
Bishal Acharya <acharya.bis@gmail.com>
2021-04-21 18:25:46 +0545

    editable polygon

--- app/src/main/java/com/teamonetech/editablepolygon/MainActivity.kt
+++ app/src/main/java/com/teamonetech/editablepolygon/MainActivity.kt
@@ -3,71 +3,128 @@ package com.teamonetech.editablepolygon
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.util.LongSparseArray
+import android.util.SparseArray
 import android.view.View
-import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.forEach
+import androidx.collection.valueIterator
 import androidx.core.content.ContextCompat
+import androidx.core.util.forEach
 import com.mapbox.geojson.Feature
 import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.LineString
 import com.mapbox.geojson.Point
-import com.mapbox.geojson.Polygon
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
-import com.mapbox.mapboxsdk.plugins.annotation.OnSymbolDragListener
-import com.mapbox.mapboxsdk.plugins.annotation.Symbol
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
-import com.mapbox.mapboxsdk.style.layers.FillLayer
+import com.mapbox.mapboxsdk.plugins.annotation.*
+import com.mapbox.mapboxsdk.style.layers.LineLayer
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.mapbox.mapboxsdk.utils.BitmapUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+fun <T> LongSparseArray<T>.getList(): List<T> {
+    val list = ArrayList<T>()
+    forEach { _, value ->
+        list.add(value)
+    }
+    return list.toList()
+}
 class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
     companion object {
         private val TAG = MainActivity::class.qualifiedName
         private val FILL_LAYER_ID = "fill-layer-id"
         private val FILL_SOURCE_ID = "fill-source-id"
         private val ID_ICON_LOCATION = "location"
+        private val ID_ICON_LOCATION_SELECTED = "location_selected"
 
     }
+
+    private lateinit var locationComponent: LocationComponent
+
     private var mapView: MapView? = null
     private var mapboxMap: MapboxMap? = null
     private var fillSource: GeoJsonSource? = null
-    val polyHashList: HashMap<String, LinkedHashMap<String, LatLng>> = HashMap()
+    var polyHashList: HashMap<String, Pair<LinkedHashMap<String, LatLng>, Boolean>> = HashMap()
     private var rootSymbolId: String? = null
     private var newPolygon: Boolean = false
+    lateinit var symbolManager: SymbolManager;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, BuildConfig.MapboxAccessToken)
+        Mapbox.getInstance(this,getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
-        Toast.makeText(this, "Click to add polygon point", Toast.LENGTH_SHORT).show()
         mapView?.getMapAsync { mapboxMap ->
             this.mapboxMap = mapboxMap
-            mapboxMap.addOnMapClickListener(this)
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+
+            mapboxMap.setStyle(Style.SATELLITE) {
                 addMarkerIconStyle(it)
+                symbolManager = SymbolManager(mapView!!, mapboxMap!!, mapboxMap?.style!!)
+                symbolManager.addClickListener(object : OnSymbolClickListener {
+                    override fun onAnnotationClick(t: Symbol?): Boolean {
+                        highlightSymbol(t)
+                        Log.d("click click", t?.id.toString());
+                        if (polyHashList.keys.first().equals(t?.id.toString())) {
+                            val polykey = polyHashList.keys.first()
+                            polyHashList.put(polykey, Pair(polyHashList.get(polykey)!!.first, true))
+                        }
+                        drawPolygon()
+                        return true
+                    }
+                })
+
+                symbolManager.addDragListener(object : OnSymbolDragListener {
+                    override fun onAnnotationDragStarted(annotation: Symbol?) {
+                        Log.d("drag","drag started")
+                        highlightSymbol(annotation)
+                        return
+                    }
+
+                    override fun onAnnotationDrag(annotation: Symbol?) {
+                        annotation?.latLng
+                        Log.d(TAG, "symbol layer id" + symbolManager.layerId)
+
+                        val polyKey = polyHashList.filter {
+                            it.value.first.filterKeys { it.equals(annotation!!.id.toString()) }.keys.size > 0
+                        }.keys.first().toString()
+                        Log.d(TAG, "drag poly key " + polyKey)
+                        polyHashList.get(polyKey)?.first?.put(annotation!!.id.toString(), annotation!!.latLng)
+
+
+                        drawPolygon()
+                        return
+                    }
+
+                    override fun onAnnotationDragFinished(annotation: Symbol?) {
+
+                        return
+                    }
+                })
+
 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
                 fillSource = initFillSource(it)
                 initFillLayer(it)
+                mapboxMap.addOnMapClickListener(this)
 
                 val position = CameraPosition.Builder()
-                    .target(LatLng(51.50550, -0.07520))
-                    .zoom(8.0)
-                    .build()
+                        .target(LatLng(51.50550, -0.07520))
+                        .zoom(8.0)
+                        .build()
 
                 mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
+
             }
 
         }
@@ -80,24 +137,38 @@ class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
 
     }
 
+    fun highlightSymbol(t:Symbol?){
+        var symbols = symbolManager.annotations
+        symbols.forEach{k,v->v.iconImage = ID_ICON_LOCATION}
+        val lists = ArrayList<Symbol>(symbols.size())
+        symbols.forEach{k,v->
+            lists.add(v) }
+
+        symbolManager.update(lists)
+        t?.iconImage = ID_ICON_LOCATION_SELECTED
+        symbolManager.update(t)
+    }
+
     private fun initFillSource(@NonNull loadedMapStyle: Style): GeoJsonSource? {
         val fillFeatureCollection =
-            FeatureCollection.fromFeatures(arrayOf())
+                FeatureCollection.fromFeatures(arrayOf())
         val fillGeoJsonSource = GeoJsonSource(FILL_SOURCE_ID, fillFeatureCollection)
         loadedMapStyle.addSource(fillGeoJsonSource)
         return fillGeoJsonSource
     }
 
     private fun initFillLayer(loadedMapStyle: Style) {
-        val fillLayer = FillLayer(
-            FILL_LAYER_ID,
-            FILL_SOURCE_ID
+        val fillLayer = LineLayer(
+                FILL_LAYER_ID,
+                FILL_SOURCE_ID
         )
         fillLayer.setProperties(
-            PropertyFactory.fillOpacity(.6f),
-            PropertyFactory.fillColor(Color.parseColor("#00e9ff"))
+                PropertyFactory.lineWidth(2f),
+                PropertyFactory.lineColor(Color.parseColor("#e55e5e"))
         )
-        loadedMapStyle.addLayer(fillLayer)
+//        loadedMapStyle.addLayer(fillLayer)
+        loadedMapStyle.addLayerBelow(fillLayer,symbolManager.layerId)
+
     }
 
 
@@ -105,58 +176,36 @@ class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
         Log.d(TAG, "Map clicked")
 
 
-        var symbolManager = SymbolManager(mapView!!, mapboxMap!!, mapboxMap?.style!!)
-
-
         var symbolOptions: SymbolOptions =
-            SymbolOptions().withLatLng(point).withIconImage(ID_ICON_LOCATION).withDraggable(true)
-                .withIconSize(2f)
+                SymbolOptions().withLatLng(point).withIconImage(ID_ICON_LOCATION).withDraggable(true)
+                        .withIconSize(1f)
+
+        val symbol = symbolManager.create(symbolOptions)
 
 
         if (polyHashList.values.isEmpty()) {
-            rootSymbolId = symbolManager.layerId
+            rootSymbolId = symbol.id.toString()
         } else if (newPolygon) {
             newPolygon = false
-            rootSymbolId = symbolManager.layerId
+            rootSymbolId = symbol.id.toString()
         }
 
-        symbolManager.create(symbolOptions)
         symbolManager.iconAllowOverlap = true
 
         if (polyHashList.get(rootSymbolId) == null) {
-            polyHashList.put(rootSymbolId!!, linkedMapOf(symbolManager.layerId to point))
+            polyHashList.put(rootSymbolId!!, Pair(linkedMapOf(symbol.id.toString() to point), false))
         } else {
-            polyHashList.get(rootSymbolId)?.put(symbolManager.layerId, point)
+            polyHashList.get(rootSymbolId)?.first?.put(symbol.id.toString(), point)
         }
 
-        Log.d(TAG, "layer id " + symbolManager.layerId)
+        Log.d(TAG, "layer id " + symbol.id.toString())
         Log.d(TAG, polyHashList.toString())
 
-        symbolManager.addDragListener(object : OnSymbolDragListener {
-            override fun onAnnotationDragStarted(annotation: Symbol?) {
-                return
-            }
-
-            override fun onAnnotationDrag(annotation: Symbol?) {
-                annotation?.latLng
-                Log.d(TAG, "symbol layer id" + symbolManager.layerId)
-
-                val polyKey = polyHashList.filter {
-                    it.value.filterKeys { it.equals(symbolManager.layerId) }.keys.size > 0
-                }.keys.first().toString()
-                Log.d(TAG, "drag poly key " + polyKey)
-                polyHashList.get(polyKey)?.put(symbolManager.layerId, annotation!!.latLng)
 
 
-                drawPolygon()
-                return
-            }
 
-            override fun onAnnotationDragFinished(annotation: Symbol?) {
 
-                return
-            }
-        })
+
 
 
         drawPolygon()
@@ -169,24 +218,26 @@ class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
     private fun drawPolygon() {
         val points = polyHashList.values
 
-        val latLngs: List<List<Point>> = points.map {
-            val items = it.values.toList().map { it -> Point.fromLngLat(it.longitude, it.latitude) }
-                .toMutableList()
-            items.add(items.get(0))
+        val latLngs: List<Point> = points.map {
+            val items = it.first.values.toList().map { it -> Point.fromLngLat(it.longitude, it.latitude) }
+                    .toMutableList()
+            if (it.second) {
+                items.add(items.get(0))
+            }
             items
-        }
+        }.first()
 
-
+        Log.d("final", latLngs.toString())
         val finalFeatureList: MutableList<Feature> = ArrayList()
-        finalFeatureList.add(Feature.fromGeometry(Polygon.fromLngLats(latLngs)))
+        finalFeatureList.add(Feature.fromGeometry(LineString.fromLngLats(latLngs)))
 
         if (fillSource != null) {
             fillSource?.setGeoJson(
-                FeatureCollection.fromFeatures(
-                    listOf(
-                        Feature.fromGeometry(Polygon.fromLngLats(latLngs))
+                    FeatureCollection.fromFeatures(
+                            listOf(
+                                    Feature.fromGeometry(LineString.fromLngLats(latLngs))
+                            )
                     )
-                )
             );
 
         }
@@ -197,14 +248,23 @@ class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
 
     private fun addMarkerIconStyle(style: Style) {
         style.addImage(
-            ID_ICON_LOCATION,
-            BitmapUtils.getBitmapFromDrawable(
+                ID_ICON_LOCATION,
+                BitmapUtils.getBitmapFromDrawable(
+                        ContextCompat.getDrawable(
+                                this,
+                                R.drawable.marker
+                        )
+                )!!,
+                false
+        )
+
+        style.addImage(ID_ICON_LOCATION_SELECTED, BitmapUtils.getBitmapFromDrawable(
                 ContextCompat.getDrawable(
-                    this,
-                    R.drawable.ic_location_on_black_24dp
+                        this,
+                        R.drawable.marker_selected
                 )
-            )!!,
-            true
+        )!!,
+                false
         )
     }
 
